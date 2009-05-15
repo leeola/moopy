8fa1608c1370e9a85fc3df9e54bbb7fac5ca1cd7
Lee Olayvar <leeolayvar@gmail.com>
2009-05-15 06:09:18 -0700

    Changed the project name, yet gain, to Moopy.

--- examples/create_cubes.py
+++ examples/create_cubes.py
@@ -1,9 +1,9 @@
 #python
 '''This example shows the creation of 3 cubes, one ontop of the other. In the
 future it will be expanded to create a small array of cubes showing different
-features of modopywrap.'''
+features of moopy.'''
 
 # Be a good boy, and do this at the beginning.
-import modopywrap.modo_session
-modopywrap.modo_session.initialize()
+import moopy.modo_session
+moopy.modo_session.initialize()
 
--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,15 +1,15 @@
 #python
-'''This example shows the base usage of modopywrap, in the good ol hello world
+'''This example shows the base usage of moopy, in the good ol hello world
 format.'''
 
 # Now we import the modo_session module so that we can call the session
 # initializer.
-import modopywrap.modo_session
+import moopy.modo_session
 
-# Now we initialize the modo session. This allows modopywrap to setup
+# Now we initialize the modo session. This allows moopy to setup
 # anything it needs, and pull any information from modo that it may need.
 # This should always be done in the beginning of a script, after imports.
-modopywrap.modo_session.initialize()
+moopy.modo_session.initialize()
 
 # Now we print. The basic print is routed into modo's event logger. This is done
 # at the initialization stage above.
