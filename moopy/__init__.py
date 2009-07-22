3f8904474aa3826cabbe2878871c28f3003c2b83
lee <lee@lees-mac-pro.local>
2009-07-21 18:56:49 -0700

    Removed the root modo library check. Not sure if its usefulness is worth the few lines it takes up.

--- moopy/__init__.py
+++ moopy/__init__.py
@@ -1,24 +1,7 @@
 ''''''
 
 # Standard
-import logging
 # Related
 # Local
-import errors
 
 
-def check_modo():
-    '''Run through a series of tests to make sure Moopy is able to
-    interact properly with modo.
-    '''
-    
-    # First we import some modo libraries, and see if it raises any errors.
-    try:
-        import lx
-    except ImportError:
-        raise errors.ModoLibrariesNotFound()
-
-if __name__ != '__main__':
-    # If the package is being imported, run the check function.
-    # The result of this, is that a check 'should' always occur
-    check_modo()
