2af8ed47602dc6b1debf51558c12f112b627db11
lee <lee@lees-mac-pro.local>
2009-09-01 20:44:05 -0700

    Added some structure to the improved versiof modo skinny. The supporting framework code is soon to come.

--- moopy_builtin/modo_skinny.py
+++ moopy_builtin/modo_skinny.py
@@ -38,5 +38,68 @@ script_options.validate_options()
 mesh_items = moopy.item.ItemCollection()
 
 # Add the active mesh items.
-mesh_items.update_selected(item_type=moopy.item.Mesh)
+mesh_items.add_selected(item_type=moopy.item.Mesh)
 
+if len(mesh_items) == 0:
+    raise NotImplemented()
+
+elif len(mesh_items) > 1:
+    raise NotImplemented()
+
+# Get the selected mesh item.
+mesh_item = mesh_items[0]
+
+def get_images(uv_map):
+    '''Get a list of images to use with this script. Returned as a list
+    of paths.'''
+    
+    image_maps = moopy.item.ItemCollection()
+    image_maps.add_selected(item_type=moopy.item.ImageMap)
+    
+    if len(image_maps) == 0:
+        raise NotImplemented()
+    
+    image_map_paths = []
+    
+    for image_map in image_maps:
+        logger.debug('Handling image map "%r"' % image_map)
+        
+        image_map_uv = image_map.uv_map
+        
+        if image_map_uv != uv_map:
+            logger.info(
+                'Not using image map "%s" because it is not using the '
+                'selected UV Map.' % image_map_uv
+            )
+            
+            image_maps.remove(image_map)
+            continue
+        
+        image_map_paths.append(image_map.clip.path)
+    
+    return image_map_paths
+
+def get_uv_map(mesh_item):
+    '''Get the uv map we are going to work with.'''
+    # Get a VMap collection of the mesh item's UVs.
+    uv_maps = moopy.vertex_maps.VMapCollection(mesh_item)
+    uv_maps.update_selected(weight=False, morph=False, other=False)
+    
+    if len(uv_maps) == 0:
+        raise NotImplemented()
+    
+    elif len(uv_maps) > 1:
+        raise NotImplemented()
+    
+    # Get the uv_map.
+    return uv_maps[0]
+
+def get_vertex_uv_positions(mesh_item):
+    '''Get the uv positions of each vertex and return a dict where the
+    keys are the vert IDs, and the keys are a tuple of U and V values.'''
+    
+    if 0:
+        # A little bit for WingIDE Autocomplete.
+        assert isinstance(mesh_item, moopy.item.Mesh)
+        
+    raise NotImplemented()
